3f425863f2778ad83f474636956abf2b70b5757e
max razhnov <shahteryshka@gmail.com>
2019-10-02 12:41:21 +0300

    feat: add webpack

---
+++ src/index.js
@@ -0,0 +1,2 @@
+import "./styles.less";
+console.log("Working...");
---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const express = require("express");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.less$/,
+        use: [
+          { loader: MiniCssExtractPlugin.loader },
+          {
+            loader: "css-loader"
+          },
+          {
+            loader: "style-loader"
+          },
+          {
+            loader: "postcss-loader"
+          },
+          {
+            loader: "less-loader",
+            options: { sourceMap: true }
+          }
+        ]
+      }
+    ]
+  },
+
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "bundle.css"
+    })
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    open: true,
+    port: 9000,
+    compress: true,
+    before: function(app, server) {
+      app.use("/api", express.static(path.join(__dirname, "src")));
+    }
+  }
+};
